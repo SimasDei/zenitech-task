6dfcc165cc23c1e2b064aa6b1aa35ab64b2420b8
SimasDei <simondeiman@gmail.com>
2019-01-29 17:41:36 +0200

    add connection to Database

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb+srv://zenitech:otul8rpKUYgS7dtC@baltic-react-mongodb-one-l0d3u.mongodb.net/zenitech-task?retryWrites=true`
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const db = require('./config/config').mongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connection to DB Established, Captain! o/'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res, next) => res.send('Ahoy there Sailor o/'));
 
